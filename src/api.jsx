9fa15bf338b6fbdeae32ec6665759359f727db56
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2025-12-12 01:42:02 +0000

    added frontend

--- src/api.jsx
+++ src/api.jsx
@@ -14,7 +14,23 @@
 //   return res.json();
 // }
 
-const API = "";
+// const API = "https://d2d9eb052cf7.ngrok-free.app";   // <-- now reading from .env
+
+// // const API = VITE_API_URL;
+
+// export async function askQuestion(query) {
+//   const res = await fetch(`${API}/ask`, {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ query }),
+//   });
+
+//   if (!res.ok) throw new Error("API error");
+
+//   return res.json();
+// }
+
+const API = "https://web-production-d8bc5.up.railway.app";
 
 export async function askQuestion(query) {
   const res = await fetch(`${API}/ask`, {
@@ -23,7 +39,10 @@ export async function askQuestion(query) {
     body: JSON.stringify({ query }),
   });
 
-  if (!res.ok) throw new Error("API error");
+  if (!res.ok) {
+    console.error("API error:", res.status);
+    throw new Error("API error");
+  }
 
   return res.json();
 }
